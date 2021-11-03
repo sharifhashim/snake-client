c662c45800184df1a55e42c05a989f889b6e4193
Hashim Sharif <sharif.hashim@gmail.com>
2021-11-03 18:42:33 +0000

    add event listener to stop program

--- play.js
+++ play.js
@@ -2,22 +2,23 @@ const net = require("net");
 
 const {connect} = require("./client")
 
-// establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: "165.227.47.243",
-//     port: 50541
-//   });
+// setup interface to handle user input from stdin
 
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-//   conn.on("data", (data) => {
-//     console.log(`${data}`)
-//   });
-//   return conn;
-// };
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    process.exit();
+  }
+}
 console.log("Connecting ...");
 
 connect();
-
+setupInput();
